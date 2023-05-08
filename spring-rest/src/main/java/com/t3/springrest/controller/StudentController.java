1bde71fc45e2cdae1e4eb1266b1d3a53e64196b4
asaph-t-Souza <asaph.net@gmail.com>
2023-05-08 20:50:16 -0300

    save

--- spring-rest/src/main/java/com/t3/springrest/controller/StudentController.java
+++ spring-rest/src/main/java/com/t3/springrest/controller/StudentController.java
@@ -3,9 +3,11 @@ package com.t3.springrest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,14 +25,24 @@ public class StudentController {
         return studentService.findAllStudents();
     }
 
-    @GetMapping("/student/{id}")
+    @GetMapping("/students/{id}")
     public Student findById(@PathVariable(name = "id") Long id) {
         return studentService.findStudentById(id);
     }
 
-    @PostMapping
+    @PostMapping("/students")
     public Student create(@RequestBody Student student) {
         return studentService.createStudent(student);
+    }
+
+    @PutMapping("/students/{id}")
+    public Student updateStudent(@PathVariable(name = "id") Long id, @RequestBody Student student){
+        return studentService.updateStudent(student, id);
+    }
+
+    @DeleteMapping("/students/{id}")
+    public void deleteStudent(@PathVariable(name = "id") Long id){
+        studentService.deleteStudentById(id);
     }
 
 }
